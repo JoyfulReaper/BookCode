b38220419db94ff17f3d91dc2593ad9d068d531a
Kyle Givler <xvegan88x@gmail.com>
2023-11-05 11:50:38 -0500

    Essential TypeScript: Chapter 2

---
+++ EssentialTypescript5_3rdEd/Chapter2/todo/src/todoItem.ts
@@ -0,0 +1,28 @@
+// This listing emphasizes the similarities between Typescript and C#, but is not the way that TS classes are usually defined
+// export class TodoItem {
+//     public id: number;
+//     public task: string;
+//     public complete: boolean;
+
+//     public constructor(id: number, task: string, complete: boolean = false) {
+//         this.id = id;
+//         this.task = task;
+//         this.complete = complete;
+//     }
+
+//     public printDetails(): void {
+//         console.log(`${this.id}\t${this.task} ${this,this.complete ? "\t(complete)": ""}`);
+//     }
+// }
+
+//Revised to use TS feat ures that allows more concise code
+export class TodoItem {
+    constructor (public id: number,
+        public task: string,
+        public complete: boolean = false) {
+    }
+
+    printDetails() : void {
+        console.log(`${this.id}\t${this.task} ${this,this.complete ? "\t(complete)": ""}`);
+    }
+}
